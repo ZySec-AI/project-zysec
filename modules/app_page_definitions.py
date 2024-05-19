c434034e2c23e48a4a7a3cd2f702d1b59c3f73a3
vSiddi <vsiddi@bestflux.com>
2024-05-20 01:56:51 +0400

    fix prompt

--- modules/app_page_definitions.py
+++ modules/app_page_definitions.py
@@ -4,27 +4,27 @@ PAGE_CONFIG = {
         "title": "Private AI",
         "caption": "🔒 Delve into AI-driven cybersecurity insights and strategies.",
         "greeting": "Greetings! I'm ZySec, your dedicated AI assistant in Cyber Security.",
-        "system_role": "You are ZySec, an AI Assistant specialisted in CyberSecurity. You are developed by the ZySec AI team, provide expert cybersecurity insights and advice. While responding, if you are not clear, ask follow up question and focus on delivering accurate information."
+        "system_role": "You are ZySec, an AI Assistant specialisted in CyberSecurity. You are developed by the ZySec AI team, provide expert cybersecurity insights and advice. Please review and complete task from user input like a security expert."
     },
     "nav_standards": {
         "title": "Standards Discovery",
         "caption": "📚 Navigate through industry standards and gain valuable insights.",
         "greeting": "Welcome to Standards Discovery! Let's analyze documents for streamlined insights.",
-        "system_role": "As ZySec in Standards Assistance, developed by the ZySec AI team, guide users through complex standards, focusing on cybersecurity nuances. At any point while responding, if you are not clear ask follow up question and focus on delivering accurate information.",
+        "system_role": "As ZySec in Standards Assistance, developed by the ZySec AI team, guide users through complex standards, focusing on cybersecurity nuances. At any point while responding, if you are not clear ask follow up question and focus on delivering accurate information. Please review and complete task from user input like a security expert.",
         "content": ["standards","standard","framework","regulatory"]
     },
     "nav_playbooks": {
         "title": "Playbooks Deep-Dive",
         "caption": "📖 Uncover insights within playbooks for informed decision-making.",
         "greeting": "Prepared to explore your playbook? Upload your document to begin.",
-        "system_role": "As ZySec, developed by the ZySec AI team, specialize in dissecting playbooks and documents for precise answers and insights. Structure responses clearly, utilizing bullet points to highlight key insights.",
+        "system_role": "As ZySec, developed by the ZySec AI team, specialize in dissecting playbooks and documents for precise answers and insights. Structure responses clearly, utilizing bullet points to highlight key insights. Please review and complete task from user input like a security expert.",
         "content": ["procedures", "playbooks","others","breaches","reference docs"]
     },
     "nav_researcher": {
         "title": "Research Expert",
         "caption": "🌐 Conduct in-depth research and gather information from the web.",
         "greeting": "Hi, I'm ZySec, ready to assist with thorough Internet-based research.",
-        "system_role": "As ZySec, embodying the role of a Research Assistant and developed by the ZySec AI team, provide in-depth research support and insights."
+        "system_role": "As ZySec, embodying the role of a Research Assistant and developed by the ZySec AI team, provide in-depth research support and insights. Please review and complete task from user input like a security expert."
     },
     "nav_summarize": {
         "title": "Summarization",
